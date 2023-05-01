dec362cdeb9b9822e2f4cb7d0fcc32f6e098fbeb
Zsombor Novozánszki <novozanszkizsombor@edu.bme.hu>
2023-05-01 12:55:42 +0200

    Extracted turkmite base

--- Turkmite/Program.cs
+++ Turkmite/Program.cs
@@ -8,7 +8,7 @@ namespace TurkMite
         static void Main(string[] args)
         {
             Mat img = new Mat(200, 200, MatType.CV_8UC3, new Scalar(0, 0, 0));
-            var turkmite = new TurkMite(img);
+            var turkmite = new OriginalTurkmite(img);
             for (int i = 0; i < 13000; i++)
             {
                 turkmite.Step();
@@ -17,19 +17,38 @@ namespace TurkMite
             Cv2.WaitKey();
         }
 
-        class TurkMite
+        class OriginalTurkmite : TurkmiteBase
         {
-            private readonly Vec3b black = new Vec3b(0, 0, 0);
-            private readonly Vec3b white = new Vec3b(255, 255, 255);
-            private readonly (int x, int y)[] delta = new (int x, int y)[] { (0, -1), (1, 0), (0, 1), (-1, 0) };
+            readonly private Vec3b black = new Vec3b(0, 0, 0);
+            readonly private Vec3b white = new Vec3b(255, 255, 255);
 
+            public OriginalTurkmite(Mat image) : base(image)
+            {
+            }
+
+            protected override Vec3b GetNextColorAndUpdateDirection(Vec3b currentColor)
+            {
+                if (currentColor == black)
+                {
+                    direction++;
+                    return white;
+                }
+                else
+                {
+                    direction--;
+                    return black;
+                }
+            }
+        }
+
+        abstract class TurkmiteBase
+        {
             public Mat Image { get; }
+            private Mat.Indexer<Vec3b> indexer;
             private int x;
             private int y;
-            private int direction;  // 0 up, 1 right, 2 down, 3 left
-            private Mat.Indexer<Vec3b> indexer;
-
-            public TurkMite(Mat image)
+            protected int direction;  // 0 up, 1 right, 2 down, 3 left
+            public TurkmiteBase(Mat image)
             {
                 Image = image;
                 x = image.Cols / 2;
@@ -37,6 +56,8 @@ namespace TurkMite
                 direction = 0;
                 indexer = image.GetGenericIndexer<Vec3b>();
             }
+
+            readonly private (int x, int y)[] delta = new (int x, int y)[] { (0, -1), (1, 0), (0, 1), (-1, 0) };
 
             public void Step()
             {
@@ -53,19 +74,7 @@ namespace TurkMite
                 y = Math.Max(0, Math.Min(Image.Rows, y));
             }
 
-            private Vec3b GetNextColorAndUpdateDirection(Vec3b currentColor)
-            {
-                if (currentColor == black)
-                {
-                    direction++;
-                    return white;
-                }
-                else
-                {
-                    direction--;
-                    return black;
-                }
-            }
+            protected abstract Vec3b GetNextColorAndUpdateDirection(Vec3b currentColor);
         }
     }
 }
